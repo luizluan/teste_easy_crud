b14bd53e6fb5078063af67d347e7c81695babaa5
Luiz Luan Beijo Assis <luizluan@outlook.com>
2018-04-25 18:39:46 -0300

    Validação de CPF, Diretiva que só permite números num input

--- CRUD_EASY.Web/App/Main/views/candidato/create/create.js
+++ CRUD_EASY.Web/App/Main/views/candidato/create/create.js
@@ -6,17 +6,56 @@
 
 
     angular.module('app').controller(controllerId,
-    [ '$location','candidatoService',
-        function ($location,candidatoService) {
+    [ '$scope','$location','candidatoService',
+        function ($scope,$location,candidatoService) {
 
             var vm = this;
 
 
 
             vm.avancar = function () {
-                candidatoService.etapa = 1;
-                $location.path("/banco");
-                return true;
+                if (vm.candidato.cpf != undefined && vm.candidato.cpf != '') {
+                    if (!validaCPF(vm.candidato.cpf)) {
+                        abp.notify.info("CPF INVÁLIDO");
+                        return;
+                    }
+                }
+                    candidatoService.etapa = 1;
+                    $location.path("/banco");
+                    return;
+            }
+
+
+            function validaCPF(cpf) {
+                var numeros, digitos, soma, i, resultado, digitos_iguais;
+                digitos_iguais = 1;
+                if (cpf.length < 11)
+                    return false;
+                for (i = 0; i < cpf.length - 1; i++)
+                    if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+                        digitos_iguais = 0;
+                        break;
+                    }
+                if (!digitos_iguais) {
+                    numeros = cpf.substring(0, 9);
+                    digitos = cpf.substring(9);
+                    soma = 0;
+                    for (i = 10; i > 1; i--)
+                        soma += numeros.charAt(10 - i) * i;
+                    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                    if (resultado != digitos.charAt(0))
+                        return false;
+                    numeros = cpf.substring(0, 10);
+                    soma = 0;
+                    for (i = 11; i > 1; i--)
+                        soma += numeros.charAt(11 - i) * i;
+                    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                    if (resultado != digitos.charAt(1))
+                        return false;
+                    return true;
+                }
+                else
+                    return false;
             }
 
 
